e8ff5a8add43e943644d8cb71b82704c7becfb73
REclipsent <wtilque@infinitymec.com>
2024-10-10 15:26:22 -0500

    Add xml documentation and static version of GetQuery

--- ParamBuilder.cs
+++ ParamBuilder.cs
@@ -6,48 +6,105 @@ namespace ParameterBuilder
 {
     public class ParamBuilder
     {
+        /// <summary>
+        ///     Dictionary of String Keys representing the query parameter and Object Values representing the value of the query parameter
+        /// </summary>
         public IDictionary<string, object> ParamsDictionary { get; set; }
+        /// <summary>
+        ///     Uri that gets appended to the front of the query parameters in certain methods 
+        /// </summary>
         public Uri BaseUri { get; set; }
+        /// <summary>
+        ///     Initialize a new instance of the ParamBuilder class.
+        /// </summary>
+        /// <param name="ParamsDictionary"></param>
+        /// <param name="BaseUri"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public ParamBuilder(IDictionary<string, object> ParamsDictionary, Uri BaseUri = null)
         {
+            if (ParamsDictionary is null) throw new ArgumentNullException(nameof(ParamsDictionary));
+
             this.ParamsDictionary = ParamsDictionary;
             this.BaseUri = BaseUri;
         }
-        public Uri GetUri() 
+        /// <summary>
+        ///     Creates Uri with passed BaseUri
+        /// </summary>
+        /// <returns>Uri of endpoint with query parameters</returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public Uri GetUri()
         {
-            if (BaseUri is null) throw new ArgumentNullException($"Define {nameof(BaseUri)} in construction or by setting the property");
+            if (BaseUri is null) throw new InvalidOperationException($"Define {nameof(BaseUri)} in construction or by setting the property");
 
             return new Uri(CreateUri(BaseUri.ToString()));
         }
+        /// <summary>
+        ///     Creates Uri with passed BaseUri
+        /// </summary>
+        /// <param name="baseUri">Uri of endpoint</param>
+        /// <returns>Uri of endpoint with query parameters</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public Uri GetUri(Uri baseUri)
         {
+            if (baseUri is null) throw new ArgumentNullException(nameof(baseUri));
+
             return new Uri(CreateUri(baseUri.ToString()));
         }
+        /// <summary>
+        ///     Creates String version of the Uri with passed BaseUri
+        /// </summary>
+        /// <returns>String version of the Uri of endpoint with query parameters</returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public string GetUriString()
         {
-            if (BaseUri is null) throw new ArgumentNullException($"Define {nameof(BaseUri)} in construction or by setting the property");
+            if (BaseUri is null) throw new InvalidOperationException($"Define {nameof(BaseUri)} in construction or by setting the property");
 
             return CreateUri(BaseUri.ToString());
         }
+        /// <summary>
+        ///     Creates Uri with passed BaseUri
+        /// </summary>
+        /// <param name="baseUri">Uri of endpoint</param>
+        /// <returns>Uri of endpoint with query parameters</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public string GetUriString(string baseUri)
         {
+            if (baseUri is null) throw new ArgumentNullException(nameof(baseUri));
+
             return CreateUri(baseUri);
         }
-
+        /// <summary>
+        /// Creates just the query part of the Uri
+        /// </summary>
+        /// <returns>Query String</returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public string GetQuery()
         {
-            return CreateQuery();
-        }
+            if (ParamsDictionary is null) throw new InvalidOperationException();
 
-        private string CreateQuery()
+            return CreateQuery(ParamsDictionary);
+        }
+        /// <summary>
+        /// Creates just the query part of the Uri
+        /// </summary>
+        /// <param name="paramsDictionary">Dictionary of String Keys representing the query parameter and Object Values representing the value of the query parameter</param>
+        /// <returns>Query String</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string GetQuery(IDictionary<string, object> paramsDictionary)
+        {
+            if (paramsDictionary is null) throw new ArgumentNullException(nameof(paramsDictionary));
+
+            return CreateQuery(paramsDictionary);
+        }
+        private static string CreateQuery(IDictionary<string, object> paramsDictionary)
         {
             StringBuilder query = new StringBuilder();
             query.Append("?");
 
-            int amountOfParams = ParamsDictionary.Count;
+            int amountOfParams = paramsDictionary.Count;
 
             int i = 1;
-            foreach (var param in ParamsDictionary)
+            foreach (var param in paramsDictionary)
             {
                 if (param.Key is null) continue;
 
@@ -68,7 +125,7 @@ namespace ParameterBuilder
             StringBuilder uri = new StringBuilder();
 
             uri.Append(baseUri);
-            uri.Append(CreateQuery());
+            uri.Append(CreateQuery(ParamsDictionary));
 
             return uri.ToString();
         }
